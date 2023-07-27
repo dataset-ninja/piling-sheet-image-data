4f779b6d06a1e8e35afdfcc1ad675b7746b7cffe
Ars Vladimirov <grokhael@gmail.com>
2023-07-28 01:28:20 +0600

    upd summary

---
+++ SUMMARY.md
@@ -0,0 +1,7 @@
+**Piling Sheet Image Data 2022** is a dataset for object detection tasks. Possible applications of the dataset could be in the construction industry. 
+
+The dataset consists of 351 images with 2430 labeled objects belonging to 2 different classes including *Dim* and *Ref*.
+
+Images in the Piling Sheet Image Data dataset have bounding box annotations. All images are labeled (i.e. with annotations). There are 2 splits in the dataset: *train* (245 images) and *val* (106 images). The dataset was released in 2022 by the [Delft University of Technology, Netherlands](https://www.tudelft.nl/en/).
+
+<img src="https://github.com/dataset-ninja/piling-sheet-image-data/raw/main/visualizations/poster.png">
--- src/convert.py
+++ src/convert.py
@@ -14,126 +14,128 @@ from tqdm import tqdm
 
 import src.settings as s
 
+# def download_dataset(teamfiles_dir: str) -> str:
+#     """Use it for large datasets to convert them on the instance"""
+#     api = sly.Api.from_env()
+#     team_id = sly.env.team_id()
+#     storage_dir = sly.app.get_data_dir()
 
-def download_dataset(teamfiles_dir: str) -> str:
-    """Use it for large datasets to convert them on the instance"""
-    api = sly.Api.from_env()
-    team_id = sly.env.team_id()
-    storage_dir = sly.app.get_data_dir()
+#     if isinstance(s.DOWNLOAD_ORIGINAL_URL, str):
+#         parsed_url = urlparse(s.DOWNLOAD_ORIGINAL_URL)
+#         file_name_with_ext = os.path.basename(parsed_url.path)
+#         file_name_with_ext = unquote(file_name_with_ext)
 
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, str):
-        parsed_url = urlparse(s.DOWNLOAD_ORIGINAL_URL)
-        file_name_with_ext = os.path.basename(parsed_url.path)
-        file_name_with_ext = unquote(file_name_with_ext)
+#         sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
+#         local_path = os.path.join(storage_dir, file_name_with_ext)
+#         teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
+#         fsize = get_file_size(local_path)
+#         with tqdm(desc=f"Downloading '{file_name_with_ext}' to buffer..", total=fsize) as pbar:
+#             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
+#         dataset_path = unpack_if_archive(local_path)
 
-        sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-        local_path = os.path.join(storage_dir, file_name_with_ext)
-        teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
-        fsize = get_file_size(local_path)
-        with tqdm(desc=f"Downloading '{file_name_with_ext}' to buffer..", total=fsize) as pbar:
-            api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
-        dataset_path = unpack_if_archive(local_path)
+#     if isinstance(s.DOWNLOAD_ORIGINAL_URL, dict):
+#         for file_name_with_ext, url in s.DOWNLOAD_ORIGINAL_URL.items():
+#             local_path = os.path.join(storage_dir, file_name_with_ext)
+#             teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
 
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, dict):
-        for file_name_with_ext, url in s.DOWNLOAD_ORIGINAL_URL.items():
-            local_path = os.path.join(storage_dir, file_name_with_ext)
-            teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
+#             if not os.path.exists(get_file_name(local_path)):
+#                 fsize = get_file_size(local_path)
+#                 with tqdm(
+#                     desc=f"Downloading '{file_name_with_ext}' to buffer {local_path}...",
+#                     total=fsize,
+#                     unit="B",
+#                     unit_scale=True,
+#                 ) as pbar:
+#                     api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
 
-            if not os.path.exists(get_file_name(local_path)):
-                fsize = get_file_size(local_path)
-                with tqdm(
-                    desc=f"Downloading '{file_name_with_ext}' to buffer {local_path}...",
-                    total=fsize,
-                    unit="B",
-                    unit_scale=True,
-                ) as pbar:
-                    api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
+#                 sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
+#                 unpack_if_archive(local_path)
+#             else:
+#                 sly.logger.info(
+#                     f"Archive '{file_name_with_ext}' was already unpacked to '{os.path.join(storage_dir, get_file_name(file_name_with_ext))}'. Skipping..."
+#                 )
 
-                sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-                unpack_if_archive(local_path)
-            else:
-                sly.logger.info(
-                    f"Archive '{file_name_with_ext}' was already unpacked to '{os.path.join(storage_dir, get_file_name(file_name_with_ext))}'. Skipping..."
-                )
-
-        dataset_path = storage_dir
-    return dataset_path
+#         dataset_path = storage_dir
+#     return dataset_path
 
 
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    dataset_path = "/home/iwatkot/supervisely/ninja-datasets/piling-sheet-image-data/02-Object_detection_data/02-Object_detection_data"
-    batch_size = 30
-    images_ext = ".jpg"
+    pass
 
-    def create_ann(image_path):
-        labels = []
 
-        image_np = sly.imaging.image.read(image_path)[:, :, 0]
-        img_height = image_np.shape[0]
-        img_wight = image_np.shape[1]
+#     dataset_path = "/home/iwatkot/supervisely/ninja-datasets/piling-sheet-image-data/02-Object_detection_data/02-Object_detection_data"
+#     batch_size = 30
+#     images_ext = ".jpg"
 
-        bbox_name = get_file_name(image_path) + ".txt"
-        bbox_path = os.path.join(curr_data_path, bbox_name)
-        if file_exists(bbox_path):
-            with open(bbox_path) as f:
-                content = f.read().split("\n")
+#     def create_ann(image_path):
+#         labels = []
 
-                for curr_data in content:
-                    if len(curr_data) != 0:
-                        curr_data = list(map(float, curr_data.split(" ")))
-                        obj_class = idx_to_obj_class[int(curr_data[0])]
+#         image_np = sly.imaging.image.read(image_path)[:, :, 0]
+#         img_height = image_np.shape[0]
+#         img_wight = image_np.shape[1]
 
-                        left = int((curr_data[1] - curr_data[3] / 2) * img_wight)
-                        right = int((curr_data[1] + curr_data[3] / 2) * img_wight)
-                        top = int((curr_data[2] - curr_data[4] / 2) * img_height)
-                        bottom = int((curr_data[2] + curr_data[4] / 2) * img_height)
-                        rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
-                        label = sly.Label(rectangle, obj_class)
-                        labels.append(label)
+#         bbox_name = get_file_name(image_path) + ".txt"
+#         bbox_path = os.path.join(curr_data_path, bbox_name)
+#         if file_exists(bbox_path):
+#             with open(bbox_path) as f:
+#                 content = f.read().split("\n")
 
-        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+#                 for curr_data in content:
+#                     if len(curr_data) != 0:
+#                         curr_data = list(map(float, curr_data.split(" ")))
+#                         obj_class = idx_to_obj_class[int(curr_data[0])]
 
-    idx_to_obj_class = {
-        0: sly.ObjClass("Dim", sly.Rectangle),
-        1: sly.ObjClass("Ref", sly.Rectangle),
-    }
+#                         left = int((curr_data[1] - curr_data[3] / 2) * img_wight)
+#                         right = int((curr_data[1] + curr_data[3] / 2) * img_wight)
+#                         top = int((curr_data[2] - curr_data[4] / 2) * img_height)
+#                         bottom = int((curr_data[2] + curr_data[4] / 2) * img_height)
+#                         rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+#                         label = sly.Label(rectangle, obj_class)
+#                         labels.append(label)
 
-    obj_classes = list(idx_to_obj_class.values())
+#         return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
 
-    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
-    meta = sly.ProjectMeta(obj_classes=obj_classes)
-    api.project.update_meta(project.id, meta.to_json())
+#     idx_to_obj_class = {
+#         0: sly.ObjClass("Dim", sly.Rectangle),
+#         1: sly.ObjClass("Ref", sly.Rectangle),
+#     }
 
-    for curr_folder in os.listdir(dataset_path):
-        ds_name = curr_folder[4:]
+#     obj_classes = list(idx_to_obj_class.values())
 
-        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+#     project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+#     meta = sly.ProjectMeta(obj_classes=obj_classes)
+#     api.project.update_meta(project.id, meta.to_json())
 
-        for curr_subfolder in os.listdir(os.path.join(dataset_path, curr_folder)):
-            curr_data_path = os.path.join(dataset_path, curr_folder, curr_subfolder)
-            if dir_exists(curr_data_path):
-                all_data = os.listdir(curr_data_path)
-                images_names = [
-                    file_name for file_name in all_data if get_file_ext(file_name) == images_ext
-                ]
+#     for curr_folder in os.listdir(dataset_path):
+#         ds_name = curr_folder[4:]
 
-                progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+#         dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
 
-                for img_names_batch in sly.batched(images_names, batch_size=batch_size):
-                    images_pathes_batch = [
-                        os.path.join(curr_data_path, image_name) for image_name in img_names_batch
-                    ]
+#         for curr_subfolder in os.listdir(os.path.join(dataset_path, curr_folder)):
+#             curr_data_path = os.path.join(dataset_path, curr_folder, curr_subfolder)
+#             if dir_exists(curr_data_path):
+#                 all_data = os.listdir(curr_data_path)
+#                 images_names = [
+#                     file_name for file_name in all_data if get_file_ext(file_name) == images_ext
+#                 ]
 
-                    img_infos = api.image.upload_paths(
-                        dataset.id, img_names_batch, images_pathes_batch
-                    )
-                    img_ids = [im_info.id for im_info in img_infos]
+#                 progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
 
-                    anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
-                    api.annotation.upload_anns(img_ids, anns_batch)
+#                 for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+#                     images_pathes_batch = [
+#                         os.path.join(curr_data_path, image_name) for image_name in img_names_batch
+#                     ]
 
-                    progress.iters_done_report(len(img_names_batch))
+#                     img_infos = api.image.upload_paths(
+#                         dataset.id, img_names_batch, images_pathes_batch
+#                     )
+#                     img_ids = [im_info.id for im_info in img_infos]
 
-    return project
+#                     anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
+#                     api.annotation.upload_anns(img_ids, anns_batch)
+
+#                     progress.iters_done_report(len(img_names_batch))
+
+#     return project
