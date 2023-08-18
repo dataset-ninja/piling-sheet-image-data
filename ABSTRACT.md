d34bdd25881859854bfdbefc226f812a803ce3f2
Ars Vladimirov <grokhael@gmail.com>
2023-08-18 22:26:50 +0600

    abstr

---
+++ ABSTRACT.md
@@ -0,0 +1,6 @@
+This is a piling sheet data used for classification and object detection. The classes are based on [Digiggids](https://digigids.hetwaterschapshuis.nl/index.php?p=gallery). Two classificiation algorithm have been made:
+
+1. Classification with four classes: Grass, Metal good, Metal bad and Rock.
+2. Classification with six classes: Grass, Metal good, Metal acceptable, Metal moderate and Metal bad and Rock.
+
+Object detection was used to detect dimensional features and estimate dimensions that could tell us about the type of piling sheet
--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Piling Sheet Image Data"
 PROJECT_NAME_FULL: str = "Piling Sheet Image Data 2022"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -87,6 +88,7 @@ def get_settings():
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
+        "hide_dataset": HIDE_DATASET,
         "applications": APPLICATIONS,
         "category": CATEGORY,
         "cv_tasks": CV_TASKS,
