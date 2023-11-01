6c6047035bcb7d8578dda26d951ff704d72b681e
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-01 13:15:27 +0600

    Update options.py

--- src/options.py
+++ src/options.py
@@ -57,6 +57,9 @@ CLASSES_PREVIEW_ROW_HEIGHT: int = None
 CLASSES_PREVIEW_PADDINGS: dict = None
 CLASSES_PREVIEW_ROWS: int = None
 CLASSES_PREVIEW_GAP: int = None
+CLASSES_PREVIEW_TAGS: int = ['dim', 'ref', 'grass', 'metal_good', 'metal_acceptable', 'metal_moderate', 'metal_bad', 'rock']
+
+
 # default {"top": "10%", "bottom": "10%", "left": "10%", "right": "10%"}
 # set % or px as string values (e.i. "10%" or "10px")
 ###############################################################
